d0e506bdad941ac04d8718e79be2842c33d073d5
suyashkelvinsavant <suyashkelvinsavant@gmail.com>
2020-07-27 22:36:58 +0530

    Javascript Inline

--- _includes/assets/js/main.js
+++ _includes/assets/js/main.js
@@ -1,7 +1,6 @@
-//Get the button:
+
 mybutton = document.getElementById("goToTop");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,7 +11,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function goToTop() {window.scroll({
     top: 0,
     left: 0,
